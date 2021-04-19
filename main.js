5dd175a79ffc5486608154c14a780760fa5052fe
Alessandro Minoli <58593668+nemolino@users.noreply.github.com>
2021-04-19 15:50:14 +0200

    speeding up detection

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 function main(){
 
 	const fs = 44100;	// [Hz]
-	const n = 10;
+	const n = 20;
 
 	// generating n seconds of random real input signal for STFT
 	//var inputReal = randomReals(n*fs);
 
 	// generate n seconds of sound signal for STFT
-	// important! to call sample() keep 1 < n < 23
+	// !important - to call sample() keep 1 < n < 23
 	var inputReal = sample(n);
 
 	// plotting input signal for STFT
-	input = document.getElementById('input');
-	Plotly.newPlot( input, [{
-	x: Array.from({length: inputReal.length}, (_, i) => i + 1),
-	y: inputReal }], {
-	margin: { t: 0 } } );
+	plotInput(inputReal);
 
 	// setting parameters for STFT (parameters used in 'Onset detection revisited', Simon Dixon)
 	const windowSize = 2048; 	// [#samples] (46ms)
 	const hopSize = 441; 		// [#samples] (10ms, 78.5% overlap)
+
+	var t0 = performance.now()
 
 	res = STFT(inputReal, windowSize, hopSize);
 	//console.log(res);
@@ -27,12 +25,11 @@ function main(){
 	df = createDetectionFunction(res, windowSize);
 	//console.log(df);
 
+	var t1 = performance.now()
+	document.getElementById("tempo").innerHTML = "Execution time: " + (t1 - t0) + " ms";
+
 	// plotting detection function
-	output = document.getElementById('output');
-	Plotly.newPlot( output, [{
-	x: Array.from({length: df.length}, (_, i) => i + 1),
-	y: df }], {
-	margin: { t: 0 } } );
+	plotDF(df);
 }
 
 function randomReals(size) {
@@ -51,22 +48,27 @@ function STFT(inputReal, windowSize, hopSize){
 	// input signals with length = windowSize+(hopSize*x)+c for x>=0 and 0<c<hopSize have their last c samples not analyzed
 
 	var res = [];
+	var zerosArray = new Array(windowSize).fill(0);
 	var i = 0;
+	var len = inputReal.length;
 
-	while (i+windowSize <= inputReal.length){
+	while (i+windowSize <= len){
 
 		// computing in-place FFT
 		var outputReal = applyWindow(inputReal.slice(i, i+windowSize), hamming);
-		var outputImag = new Array(windowSize).fill(0);	// inputImag filled with zeros
+		var outputImag = zerosArray.slice();	// inputImag filled with zeros
 		transform(outputReal, outputImag);
 
 		// in outputReal Re[-f] =  Re[f] 	(outputReal is symmetric respect to the element at index windowSize/2)
 		// in outputImag Im[-f] = -Im[f]
+		// using only the positive side of the spectrum makes the detection function sharper and reduces the computational time significantly
+		outputReal = outputReal.slice(windowSize/2, windowSize);
+		outputImag = outputImag.slice(windowSize/2, windowSize);
 
 		// converting output to polar coordinates
 		var outputR = [];
 		var outputPhi = [];
-		for (var j = 0; j < windowSize; j++){
+		for (var j = 0; j < windowSize/2; j++){
 			var complex = math.complex(outputReal[j], outputImag[j]);
 		    outputR.push(complex.abs());
 		    outputPhi.push(complex.arg());	// phase is in (-pi, pi] range
@@ -86,18 +88,20 @@ function createDetectionFunction(s, windowSize){
 
 	var targetAmplitudes = [];
 	var targetPhases = [];
+	var zerosArray = new Array(windowSize/2).fill(0);
 
 	// target amplitude for the 1st frame is set to 0
-	targetAmplitudes.push(new Array(windowSize).fill(0));
+	targetAmplitudes.push(zerosArray.slice());
 
 	// target amplitude for the 2nd frame (it's here only to avoid pushing amplitude and phase values in two separate loops)
 	targetAmplitudes.push(s[0][0]);
 
 	// target phase for the 1st and the 2nd frame is set to 0
-	targetPhases.push(new Array(windowSize).fill(0));
-	targetPhases.push(new Array(windowSize).fill(0));	
+	targetPhases.push(zerosArray.slice());
+	targetPhases.push(zerosArray.slice());	
 
-	for (var i = 2; i < s.length; i++){
+	var len = s.length;
+	for (var i = 2; i < len; i++){
 		targetAmplitudes.push(s[i-1][0]);
 		targetPhaseValue = math.subtract(math.multiply(2, s[i-1][1]), s[i-2][1]);
 		// given a vector x, computing math.atan2(math.sin(x), math.cos(x)) maps the values of x to the range [-pi, pi]
@@ -109,10 +113,10 @@ function createDetectionFunction(s, windowSize){
 
 	// constructing the detection function
 	var detectionFunction = [];
-	for (var i = 0; i < s.length; i++){
+	for (var i = 0; i < len; i++){
 
 		var stationarityMeasures = [];
-		for (var k = 0; k < windowSize; k++){
+		for (var k = 0; k < windowSize/2; k++){
 
 			var targetReIm = math.Complex.fromPolar({r: targetAmplitudes[i][k], phi: targetPhases[i][k]});
 			var measuredReIm = math.Complex.fromPolar({r: s[i][0][k], phi: s[i][1][k]});
