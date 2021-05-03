32a041ead54fe92141b1a1703b66bf96ee689d73
Alessandro Minoli <58593668+nemolino@users.noreply.github.com>
2021-05-03 17:02:48 +0200

    Finishing peak-picking

---
+++ plots.js
@@ -0,0 +1,66 @@
+function plotData1(inputReal, df){
+	var trace1 = {
+	  x: Array.from({length: inputReal.length}, (_, i) => i + 1),
+	  y: inputReal,
+	  type: 'scatter',
+	  name: 'input signal',
+	  line: {color: 'rgb(200, 200, 200)'}
+	};
+	var trace2 = {
+	  x: Array.from({length: inputReal.length}, (_, i) => (i + 1) * 441),
+	  y: df,
+	  type: 'scatter',
+	  name: 'detection function',
+	  line: {color: 'rgb(55, 128, 191)'}
+	};
+	var data = [trace1, trace2];
+	var layout = {
+	  title:'Detection function with input signal',
+	  yaxis: {range: [-1, 1]}
+	};
+	Plotly.newPlot('result', data, layout);
+}
+
+function plotData2(df, threshold){
+	var trace1 = {
+	  x: Array.from({length: df.length}, (_, i) => i + 1),
+	  y: df,
+	  type: 'scatter',
+	  name: 'detection function',
+	  line: {color: 'rgb(55, 128, 191)'}
+	};
+	var trace2 = {
+	  x: Array.from({length: threshold.length}, (_, i) => i + 1),
+	  y: threshold,
+	  type: 'scatter',
+	  name: 'threshold',
+	  line: {dash: 'dot', color: 'rgb(255, 0, 0)'}
+	};
+	var data = [trace1, trace2];
+	var layout = {
+	  title:'Detection function with adaptive threshold'
+	};
+	Plotly.newPlot('df-threshold', data, layout);
+}
+
+function plotData3(df, localMaximums){
+	var trace1 = {
+	  x: Array.from({length: df.length}, (_, i) => i + 1),
+	  y: df,
+	  type: 'scatter',
+	  name: 'detection function',
+	  line: {color: 'rgb(55, 128, 191)'}
+	};
+	var trace2 = {
+	  x: localMaximums[0],
+	  y: localMaximums[1],
+	  mode: 'markers',
+	  type: 'scatter',
+	  name: 'local maximum'
+	};
+	var data = [trace1, trace2];
+	var layout = {
+	  title:'Detection function minus threshold with local maximums > 0'
+	};
+	Plotly.newPlot('df-maximums', data, layout);
+}
